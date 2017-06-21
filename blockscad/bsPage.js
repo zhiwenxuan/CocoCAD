a76c9e50ef27f8f04b4f345756ae502762ea7318
zcm0705 <cuimei@brandeis.edu>
2017-06-21 18:34:51 +0800

    render button

--- blockscad/bsPage.js
+++ blockscad/bsPage.js
@@ -251,7 +251,7 @@ output += '                </div>\n';   // end of viewerDefaults div
 
 
 output += '                <div id="renderPane">\n';
-output += '                  <button type="button" class="btn btn-default btn-lg changeable" id="renderButton">' + Blockscad.Msg.RENDER_BUTTON + '</button>\n';
+output += '                  <button type="button" class="btn btn-default btn-lg changeable mdl-button mdl-js-ripple-effect mdl-js-button mdl-button--fab mdl-color--green" id="renderButton"><i class="material-icons mdl-color-text--white" style="font-size: 30px">play_arrow</i></button>\n';
 output += '                  <button type="button" class="btn btn-default btn-lg btn-danger " id="abortButton">' + Blockscad.Msg.ABORT_BUTTON + '</button>\n';
 output += '                  <div id="stl_buttons" class="pull-right" style="padding:5px 5px;">\n';
 output += '                    <select id="render-type" style="padding:2px 4px;"></select>\n';
